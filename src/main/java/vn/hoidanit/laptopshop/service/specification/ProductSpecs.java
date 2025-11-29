77d32062d16a848b74c1fb3b3f5578ed6465df9d
Dang The Hao <dangthehao28@gmail.com>
2025-11-29 16:24:53 +0700

    fix tạm lỗi Hibernate JPA model gen

--- src/main/java/vn/hoidanit/laptopshop/service/specification/ProductSpecs.java
+++ src/main/java/vn/hoidanit/laptopshop/service/specification/ProductSpecs.java
@@ -9,37 +9,37 @@ import java.util.List;
 public class ProductSpecs {
 
     public static Specification<Product> nameLike(String name) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Product_.NAME), "%" + name + "%");
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get("name"), "%" + name + "%");
     }
 
     public static Specification<Product> minPrice(double minPrice) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.ge(root.get(Product_.PRICE), minPrice);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.ge(root.get("price"), minPrice);
     }
 
     public static Specification<Product> maxPrice(double maxPrice) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.lessThan(root.get(Product_.PRICE), maxPrice);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.lessThan(root.get("price"), maxPrice);
     }
 
     public static Specification<Product> factoryEquals(String factoryName) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Product_.FACTORY), factoryName);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("factory"), factoryName);
     }
 
     public static Specification<Product> factoriesIn(List<String> factories) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get(Product_.FACTORY)).value(factories);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get("factory")).value(factories);
     }
 
     public static Specification<Product> priceInRange(double minPrice, double maxPrice) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.and(
-                criteriaBuilder.ge(root.get(Product_.PRICE), minPrice),
-                criteriaBuilder.le(root.get(Product_.PRICE), maxPrice));
+                criteriaBuilder.ge(root.get("price"), minPrice),
+                criteriaBuilder.le(root.get("price"), maxPrice));
     }
 
     public static Specification<Product> priceInRanges(double minPrice, double maxPrice) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.between(root.get(Product_.PRICE), minPrice, maxPrice);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.between(root.get("price"), minPrice, maxPrice);
     }
 
     public static Specification<Product> targetsIn(List<String> targets) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get(Product_.TARGET)).value(targets);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.in(root.get("target")).value(targets);
     }
 
     public static Specification<Product> dij() {
